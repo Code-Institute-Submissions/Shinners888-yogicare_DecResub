874724fdbca00ef07b93c4b243ffde7632900ff3
Shinners888 <sinead888888888@gmail.com>
2021-08-12 21:33:59 +0000

    added class detail page

--- classes/urls.py
+++ classes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_classes, name='classes'),
+    path('<classe_id>', views.classe_detail, name='classe_detail'),
 ]
--- classes/views.py
+++ classes/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from .models import Classes
 
 
 def all_classes(request):
-    """ A view to show all classes, including sorting and search queries """
 
     classes = Classes.objects.all()
 
@@ -13,3 +12,14 @@ def all_classes(request):
     }
 
     return render(request, 'classes/classes.html', context)
+
+
+def classe_detail(request, classe_id):
+
+    classe = get_object_or_404(Classes, pk=classe_id)
+
+    context = {
+        'classe': classe,
+    }
+
+    return render(request, 'classes/classe_detail.html', context)
