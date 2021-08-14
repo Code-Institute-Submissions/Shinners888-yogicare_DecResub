c4f4565a528febccd95c8c395a71a7809fbf3ba4
Shinners888 <sinead888888888@gmail.com>
2021-08-14 21:55:23 +0000

    added views and urls for shop

--- shop/models.py
+++ shop/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 
-class Shop(models.Model):
+class Item(models.Model):
 
     class Meta:
-        verbose_name_plural = 'Shop'
+        verbose_name_plural = 'Items'
 
-    name = models.CharField(max_length=254)
+    name = models.CharField(max_length=254, null=True, blank=True)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     price = models.URLField(max_length=1024, null=True, blank=True)
-    colours = models.CharField(max_length=254)
+    colours = models.CharField(max_length=254, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.conf import settings
+from .models import Item
 
-# Create your views here.
+
+def all_items(request):
+
+    items = Item.objects.all()
+
+    context = {
+        'items': items,
+    }
+
+    return render(request, 'shop/items.html', context)
+
+
+def item_detail(request, item_id):
+
+    item = get_object_or_404(Item, pk=item_id)
+
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'shop/item_detail.html', context)
