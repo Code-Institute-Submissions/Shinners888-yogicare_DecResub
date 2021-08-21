ead752a7e2ea179b630a992d219bb7f12ef23513
Shinners888 <sinead888888888@gmail.com>
2021-08-21 17:30:55 +0000

    added colours to items

--- shop/models.py
+++ shop/models.py
@@ -9,9 +9,24 @@ class Item(models.Model):
     name = models.CharField(max_length=254, null=True, blank=True)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    colours = models.CharField(max_length=254, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     description = models.CharField(max_length=254, null=True, blank=True)
+    colours = models.BooleanField(default=False, null=True, blank=True)
+    if colours:
+        black = models.BooleanField(default=False)
+        pink = models.BooleanField(default=False)
+        yellow = models.BooleanField(default=False)
+        red = models.BooleanField(default=False)
+        green = models.BooleanField(default=False)
+        blue = models.BooleanField(default=False)
+    else:
+        black = False
+        pink = False
+        yellow = False
+        red = False
+        green = False
+        blue = False
+        
 
     def __str__(self):
         return self.name
