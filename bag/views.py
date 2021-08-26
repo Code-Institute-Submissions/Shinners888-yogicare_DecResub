9c705bcca6584fe9f1ea0bb37623c77dfb032ee4
Shinners888 <sinead888888888@gmail.com>
2021-08-26 23:58:21 +0000

    added adjust/delete functions to bag, changed models for classes/categories

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 
 from shop.models import Item
@@ -37,6 +37,53 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    colour = None
+    if 'item_colour' in request.POST:
+        colour = request.POST['item_colour']
+    bag = request.session.get('bag', {})
+
+    if colour:
+        if quantity > 0:
+            bag[item_id]['items_by_colour'][colour] = quantity
+        else:
+            del bag[item_id]['items_by_colour'][colour]
+            if not bag[item_id]['items_by_colour']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('shopping_bag'))
+
+
+def remove_from_bag(request, item_id):
+
+    try:
+        colour = None
+        if 'item_colour' in request.POST:
+            colour = request.POST['item_colour']
+        bag = request.session.get('bag', {})
+
+        if colour:
+            del bag[item_id]['items_by_colour'][colour]
+            if not bag[item_id]['items_by_colour']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
 
 
 def add_classe_to_bag(request, classe_id):
--- classes/models.py
+++ classes/models.py
@@ -8,7 +8,6 @@ class Category(models.Model):
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
@@ -19,11 +18,14 @@ class Category(models.Model):
 
 
 class Classes(models.Model):
+    class Meta:
+        verbose_name_plural = 'Classes'
+
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     description = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-
+    image = models.ImageField(null=True, blank=True)
     def __str__(self):
         return self.name
