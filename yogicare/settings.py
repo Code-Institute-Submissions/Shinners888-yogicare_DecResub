86766eef8e50e808475c15480e862191c35bf11d
Shinners888 <sinead888888888@gmail.com>
2021-09-03 09:16:49 +0000

    changed secret key and set debug back to false

--- yogicare/settings.py
+++ yogicare/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-03@b__7&r(-$^(amp0#g3i!n9*f+x=4y0qz7lojhqsf8s*1fir'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['yogicare.herokuapp.com', 'localhost']
 
